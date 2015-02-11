38aa8130098a9db5ddac339ea0bafabcca05195c
Jen Whitney <jen.j.whitney@gmail.com>
2015-02-11 14:02:12 -0500

    Export to Refworks: clean up code & combine with JQuery customizations in bib display

--- bibdisplay_local.js
+++ bibdisplay_local.js
@@ -16,6 +16,23 @@ $(document).ready(function() {
     };
     return attrValue + '?' + jQuery.param(params);
   });
+  // "Export to RefWorks" link directs users to Carleton's Refworks login page.
+  // Update link to export current record to Refworks.
+  $('a#export-refworks').attr('href', function(i, attrValue) {
+    return "javascript:open_refworks_window('" + $('a#recordnum').attr('href').replace(/^.+?(b\d+)$/, "$1") + "')";
+  });
 });
 
+/**
+ * Condensed version of IUG community export-to-RefWorks function.
+ * @param {string} bibnum Bibliographic record ID
+ */
+function open_refworks_window(bibnum) {
+  // Build Refworks import URL:
+  var url = 'http://proxy.library.carleton.ca/login?url=' +
+          'http://refworks.scholarsportal.info/express/expressimport.asp?vendor=Carleton%20University&filter=Refworks%20Tagged%20Format&url=' +
+          'http://refworks.library.carleton.ca/cgi-bin/refworks/refworks.php?bibnum=' + bibnum;
 
+  new_refworks_win=window.open(url,"RefWorksMain",'toolbar=1,location=1,directories=0,status=1,menubar=1,scrollbars=1,resizable=1,width=800,height=500');
+  new_refworks_win.focus();
+}
