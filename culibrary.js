94ea34e11682d3f4defc6eba17b75969888fecae
Jen Whitney <jen.j.whitney@gmail.com>
2017-10-11 11:13:10 -0400

    Remove RefWorks code. See #30

--- culibrary.js
+++ culibrary.js
@@ -1,5 +1,5 @@
 /**
- * JQuery-powered customizations of keyword search results, browse lists, and 
+ * JQuery-powered customizations of keyword search results, browse lists, and
  * bib/eresource record display pages.
  */
 $(document).ready(function() {
@@ -28,7 +28,7 @@ $(document).ready(function() {
         $('~ tr.browseSubEntry', this).slice(0,(rowspan-1)).addClass('odd');
     }
   });
-  
+
   /**
    * Bib & eresource record display customizations.
    */
@@ -37,13 +37,13 @@ $(document).ready(function() {
   $('#bibMediaIcon').append( $('#bibMedia img') );
   $('#bibDisplayContent').find('.bibDetail:first tr:first').append('<td id="bibJacketIcon" class="bibIcon"></td>');
   $('#bibJacketIcon').append( $('#bibDisplayJacket img') );
-  
+
   // Remove default port & all scopes from persistent record link
   $("a#recordnum").attr('href', function(i, attrValue) {
     return attrValue.replace(/~S\d+$/,'');
   });
   $("a#recordnum").text($("a#recordnum").text().replace(/\:80\//, '/').replace(/~S\d+$/,''));
-  
+
   // Add title & call number as query parameters to 'Report Missing Item' form
   $("a#form-missing-item").attr('href', function(i, attrValue) {
     var params = {
@@ -52,23 +52,4 @@ $(document).ready(function() {
     };
     return attrValue + '?' + jQuery.param(params);
   });
-  // "Export to RefWorks" link directs users to Carleton's Refworks login page.
-  // Update link to export current record to Refworks.
-  $('a#export-refworks').attr('href', function(i, attrValue) {
-    return "javascript:open_refworks_window('" + $('a#recordnum').attr('href').replace(/^.+?(b\d+)$/, "$1") + "')";
-  });
 });
-
-/**
- * Condensed version of IUG community export-to-RefWorks function.
- * @param {string} bibnum Bibliographic record ID
- */
-function open_refworks_window(bibnum) {
-  // Build Refworks import URL:
-  var url = 'http://proxy.library.carleton.ca/login?url=' +
-          'http://refworks.scholarsportal.info/express/expressimport.asp?vendor=Carleton%20University&filter=Refworks%20Tagged%20Format&url=' +
-          'http://refworks.library.carleton.ca/cgi-bin/refworks/refworks.php?bibnum=' + bibnum;
-
-  new_refworks_win=window.open(url,"RefWorksMain",'toolbar=1,location=1,directories=0,status=1,menubar=1,scrollbars=1,resizable=1,width=800,height=500');
-  new_refworks_win.focus();
-}
